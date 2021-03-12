cd61d0d633210ba1f4df2e6c766229c3dae7c30b
chanakya2504 <76518448+chanakya2504@users.noreply.github.com>
2021-03-12 11:16:19 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var superhero,
+
+function preload() {
+//preload the images here
+
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+
